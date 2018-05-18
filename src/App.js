9f68a114341e17db8cd7038c2f261af8e6409c5b
Ricardo Peña-Castro <ricardo@Ricardos-MBP.home>
2018-05-18 01:12:31 -0400

    route examples

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Signs from './pages/Signs.jsx';
 import Simulation from './pages/Simulation.jsx';
 
 import Example2 from './components/Example2.jsx';
+import Example3 from './components/Example3.jsx';
+import Example4 from './components/Example4.jsx';
+import Example5 from './components/Example5.jsx';
+import Example6 from './components/Example6.jsx';
+import Example7 from './components/Example7.jsx';
 
 class App extends Component {
   render() {
@@ -17,6 +22,11 @@ class App extends Component {
           <Route path="/simulation" component={Simulation}/>
 
           <Route path="/example2" component={Example2}/>
+          <Route path="/example3" component={Example3}/>
+          <Route path="/example4" component={Example4}/>
+          <Route path="/example5" component={Example5}/>
+          <Route path="/example6" component={Example6}/>
+          <Route path="/example7" component={Example7}/>
         </div>
       </Router>
     );
---
+++ src/components/Example5.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Jumbotron from '../components/Jumbotron.jsx';
+import { Link } from 'react-router-dom';
+
+class Example5 extends Component {
+  render() {
+    return (
+      <div>
+      	<Navbar />
+      	<Jumbotron title="Simulation" subtitle="Credit: hacksplaining.com"/>
+
+        <div className="container" id="simulation">
+          <Link to="/example6" id="link">Next</Link>
+      	 <img className="center" src={ require('../images/example3.png') } />
+      	 <p>
+            img result
+		    </p>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Example5;
